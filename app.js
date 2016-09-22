92168f09fe1332bb06871cad748409a0e39c8d4c
Sara <snsimpson1202@gmail.com>
2016-09-22 12:41:06 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $(".dropdown img").click(function(){
+    $(".dropdown-menu").toggle();
+  });
+  $("form").submit(function(){
+    var email = $("#email").val();
+    var password = $("#password").val();
+    
+    if (email === "") {
+      $(".email-error").text("Please enter your email.");
+    }
+    
+    if (password === "") {
+      $(".password-error").text("Please enter your password.");
+    }
+     return false;
+  }); 
+});
